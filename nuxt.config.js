3640df9a8f0e6f683a2ce6f1d490cb22245de15f
imshubhamcodex <42334452+imshubhamcodex@users.noreply.github.com>
2020-08-01 21:35:04 +0530

    Add files via upload

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,6 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'server',
-  
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
